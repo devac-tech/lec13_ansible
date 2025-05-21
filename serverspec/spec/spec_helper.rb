41b9c15c0d2ac66ec63eb3bd57d4514adc995816
devac-tech <sigoto.yamano@gmail.com>
2025-05-21 16:43:32 +0900

    add serverspec exe file

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -1,6 +1,3 @@
-require 'serverspec'
-
-set :backend, :ssh
 host = ENV['TARGET_HOST'] || 'target'
 
 options = Net::SSH::Config.for(host)
@@ -9,3 +6,35 @@ options[:keys] = ['~/.ssh/id_rsa']  # CircleCIに登録した鍵パス
 set :host, host
 set :ssh_options, options
 set :request_pty, true
+
+
+require 'serverspec'
+require 'net/ssh'
+
+# テストの実行先は「リモート（SSH）」
+set :backend, :ssh
+
+# sudo パスワードの取得処理
+if ENV['ASK_SUDO_PASSWORD']
+  begin
+    require 'highline/import'
+  rescue LoadError
+    fail "highline is not available. Try installing it."
+  end
+  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
+else
+  set :sudo_password, ENV['SUDO_PASSWORD']
+end
+
+# ホストと SSH 接続オプション
+host = ENV['TARGET_HOST']
+
+# ~/.ssh/config に設定があればそれを自動的に読み込む
+options = Net::SSH::Config.for(host)
+
+# ユーザー指定がなければ、現在ログイン中のユーザー名を使
+options[:user] ||= Etc.getlogin
+
+# 実際に接続するホスト名（IP）と、SSHのオプションを Serverspec に渡す
+set :host,        options[:host_name] || host
+set :ssh_options, options
