c633c152c01e85cc52315de1b2546952605dd8b7
devac-tech <sigoto.yamano@gmail.com>
2025-05-22 02:12:19 +0900

    fix serverspec file

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -1,23 +1,12 @@
 require 'serverspec'
 require 'net/ssh'
-require 'etc'
 
-# テスト対象はリモート（SSH接続）
 set :backend, :ssh
-
-# sudoを無効化（NOPASSWD 環境のため）
 set :disable_sudo, true
 
-# 必須の環境変数が設定されているかを検証
 host = ENV['TARGET_HOST']
 raise 'TARGET_HOST is not set' if host.nil? || host.strip.empty?
 
-# ~/.ssh/config の内容を読み込み、必要な接続設定を取得
-options = Net::SSH::Config.for(host)
-
-# SSHユーザー未設定なら、現在のユーザーを使用
-options[:user] ||= Etc.getlogin
-
-# Serverspec に接続情報を渡す
+options = Net::SSH::Config.for('target') # ← SSH config で定義した "target" を使う
 set :host,        options[:host_name] || host
 set :ssh_options, options
