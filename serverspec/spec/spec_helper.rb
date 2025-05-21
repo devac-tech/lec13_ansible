879a7996c7de7e0107512ca8cec341c7a556d58d
devac-tech <sigoto.yamano@gmail.com>
2025-05-21 22:14:13 +0900

    fix serverspec file

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -1,13 +1,3 @@
-host = ENV['TARGET_HOST'] || 'target'
-
-options = Net::SSH::Config.for(host)
-options[:user] = 'ec2-user'
-options[:keys] = ['~/.ssh/id_rsa']  # CircleCIに登録した鍵パス
-set :host, host
-set :ssh_options, options
-set :request_pty, true
-
-
 require 'serverspec'
 require 'net/ssh'
 
